dd5bf7c05e3df05bb27d09a9685a22820ce552b1
stuhops <stwhop@gmail.com>
2020-09-02 22:15:30 -0600

    Restarted the vue process

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,26 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Index from '@/components/Index'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
 
-Vue.use(Router)
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
+  }
+];
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Index',
-      component: Index
-    }
-  ]
-})
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+});
+
+export default router;
