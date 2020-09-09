0fc61f0d84a387f49aea7ffc27db5f6f5d1bc7af
stuhops <stwhop@gmail.com>
2020-09-08 21:41:12 -0600

    Added the landing header

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <nav class="navbar navbar-expand-sm navbar-light">
+  <nav id="nav">
     <!-- Links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link 1</a>
+    <ul id="nav-link-custom-section" class="nav-section">
+      <li class="nav-link-custom">
+        <a class="nav-link-custom" href="#"></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link 2</a>
+      <li class="nav-link-custom">
+        <a class="nav-link-custom" href="#"></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link 3</a>
+      <li class="nav-link-custom">
+        <a class="nav-link-custom" href="#"></a>
       </li>
     </ul>
   </nav>
@@ -25,6 +25,12 @@
     url("./assets/fonts/Crimson-Semibold.otf") format("opentype");
 }
 
+@font-face {
+  font-family: "ProximaNova";
+  src: local("ProximaNova"),
+    url("./assets/fonts/ProximaNova-Regular.otf") format("opentype");
+}
+
 * {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -32,7 +38,104 @@
   color: #f5f5f5;
 }
 
+body {
+  margin: 0px;
+}
+
 h1, h2, h3, h4, h5, h6 {
   font-family: Crimson, serif;
+}
+
+p, a, li {
+  font-family: ProximaNova, sans-serif;
+  font-size: 16px;
+}
+
+#nav {
+  margin-bottom: -96px;
+  display: flex;
+  flex-direction: row;
+}
+
+#nav-link-custom-section {
+  justify-content: center;
+}
+
+
+.borderless-button-dark {
+  border-style: none;
+  background-color: transparent;
+  color: #202020;
+  text-shadow: 0px 0px 10px #f5f5f5;
+  text-shadow: 0px 0px 10px #f5f5f5;
+  padding: 10px 20px 10px 20px;
+}
+
+.borderless-button-light {
+  border-style: none;
+  background-color: transparent;
+  color: #f5f5f5;
+  text-shadow: 0px 0px 10px #202020;
+  text-shadow: 0px 0px 10px #202020;
+  padding: 10px 20px 10px 20px;
+}
+
+.borderless-button-light:hover, .borderless-button-dark:hover {
+  opacity: 0.7;
+}
+
+.outlined-button-dark {
+  border-style: solid;
+  border-color: #202020;
+  background-color: transparent;
+  color: #202020;
+  text-shadow: 0px 0px 10px #f5f5f5;
+  text-shadow: 0px 0px 10px #f5f5f5;
+  padding: 20px 40px 20px 40px;
+}
+
+.outlined-button-dark:hover {
+  border-color: #f5f5f5;
+  background-color: #202020;
+  color: #f5f5f5;
+  text-shadow: none;
+}
+
+.outlined-button-light {
+  border-style: solid;
+  border-color: #f5f5f5;
+  background-color: transparent;
+  color: #f5f5f5;
+  text-shadow: 0px 0px 10px #202020;
+  text-shadow: 0px 0px 10px #202020;
+  padding: 20px 40px 20px 40px;
+}
+
+.outlined-button-light:hover {
+  border-color: #202020;
+  background-color: #f5f5f5;
+  color: #202020;
+  text-shadow: none;
+}
+
+.nav-section {
+  height: 96px;
+  max-width: 1396px;
+  margin: 0 10vw 0 10vw;
+  padding: 0;
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+.nav-link-custom {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  font-size: 1.5rem;
+  color: #f5f5f5;
+  text-shadow: 0px 0px 10px #000000;
+  text-shadow: 0px 0px 10px #000000;
 }
 </style>
--- src/components/LandingHeader.vue
+++ src/components/LandingHeader.vue
@@ -1,37 +1,43 @@
+// TODO: Make #header-text take the prop header and break on a new line
 <template>
   <div class="main-container" :style="{ backgroundImage: 'url(' + background_image + ')' }">
     <div class="container">
-      <h1 id="header-text">{{ header }}</h1>
+      <h1 id="header-text">Full-Stack<br />Developer.</h1>
+      <div class="links">
+        <a>
+          <button class="outlined-button-light">Contact Me Today</button>
+          <button class="borderless-button-light">Download Resume</button>
+        </a>
+      </div>
     </div>
   </div>
 </template>
-
 <script>
 export default {
   name: "LandingHeader",
   props: {
     background_image: String,
     header: String
-  },
+  }
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 #header-text {
+  flex: 1;
   font-size: clamp(72px, 8vw, 120px);
-  white-space: pre-wrap;
+  margin: 0;
 }
 
 .main-container {
   width: 100vw;
   height: 100vh;
-  background-color: green;
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center top;
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
+  align-items: center;
 }
 
 .container {
