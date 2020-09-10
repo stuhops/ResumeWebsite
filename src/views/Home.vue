c0b118b6393e5b1f615c7499382b94576d643829
stuhops <stwhop@gmail.com>
2020-09-09 18:39:27 -0600

    Removed references to Single Statement Section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,22 +3,17 @@
     header="Full-Stack\nDeveloper."
     :background_image="LandingHeaderImage"
   />
-  <SingleStatementSection
-    headline="My passion and focus is Full-Stack web development"
-  />
 </template>
 
 <script>
 import LandingHeader from "@/components/LandingHeader.vue";
-import SingleStatementSection from "@/components/SingleStatementSection.vue";
 
 import LandingHeaderImage from "@/assets/img/landing-page/welcome.jpg";
 
 export default {
   name: "Home",
   components: {
-    LandingHeader,
-    SingleStatementSection
+    LandingHeader
   },
   data: function() {
     return {
